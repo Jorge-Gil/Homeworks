4aa03d4a5ad7a1970d06ca6adb6ce239c0443683
Jorge-Gil <71947798+Jorge-Gil@users.noreply.github.com>
2023-02-20 21:10:29 +0000

    Creado el proyecto del taller

--- React/reacttest/src/GifGrid.js
+++ React/reacttest/src/GifGrid.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useEffect } from "react";
+import { useState } from "react";
 
 const getGifs = async (category) => {
-  const url =
-  `https://api.giphy.com/v1/gifs/search?api_key=Lqw9Rv7MajHLSgoLdOI8s8oQvJkkijt2&q=${category}&limit=25&offset=0&rating=pg-13&lang=en`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=Lqw9Rv7MajHLSgoLdOI8s8oQvJkkijt2&q=${category}&limit=25&offset=0&rating=pg-13&lang=en`;
   const resp = await fetch(url);
   const { data } = await resp.json();
   const gifs = data.map((img) => {
@@ -17,16 +17,43 @@ const getGifs = async (category) => {
   return gifs;
 };
 
-const GifGrid = ({ category }) => {
-  useEffect(() => {
-    console.log(category);
-  }, []);
+const addImages = (images) => {
+  const img = images.map((img) => {
+    return (
+      <div>
+        <h2> {img.title}</h2>
+        <li key={img.id}>
+          <img src={img.url} alt={img.title} />
+        </li>
+      </div>
+    );
+  });
+  return img;
+};
 
-    getGifs(category);
+const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    getGifs(category).then((gifs) => setImages(gifs));
+    console.log(category);
+  }, [category]);
+
+  getGifs(category);
+
+  const Gifs = async () => {
+    const gifs = await getGifs(category);
+    setImages(gifs);
+    console.log(gifs);
+  };
+  Gifs.then((gifs) => setImages(gifs));
+
   return (
     <>
       <h3> {category}</h3>
       <p>Hello world</p>
+      <ul>{addImages(images)}</ul>
+      <ul> {Gifs} </ul>
     </>
   );
 };
