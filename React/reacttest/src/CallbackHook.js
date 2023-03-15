6e69556bf014f2ffc194b2d431d1d71969767a3f
Jorge-Gil <71947798+Jorge-Gil@users.noreply.github.com>
2023-03-15 00:01:55 +0000

    CallbackHook y ShowIncrement

---
+++ React/reacttest/src/CallbackHook.js
@@ -0,0 +1,17 @@
+import React, {useCallback, useState} from 'react'
+import { ShowIncrement } from './ShowIncrement'
+
+export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10)
+
+  const incrementFather = useCallback(() => { 
+    console.log('calling setCounter')
+    setCounter((value) => value + 1)
+  }, [])
+    return (
+      <>
+    <div>CallbackHook {counter}</div>
+    <ShowIncrement incrementFather={incrementFather} />
+    </>
+  )
+}
