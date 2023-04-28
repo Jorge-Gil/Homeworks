9ea8beefd1ca8b3eb3656cd60099809014f8f8c5
Jorge-Gil <71947798+Jorge-Gil@users.noreply.github.com>
2023-04-28 00:48:38 +0000

    contraseña hasheada con bcrypt y login

--- backend/Controllers/auth.js
+++ backend/Controllers/auth.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
-const  Usuario  = require("../models/Usuario");
-
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/Usuario");
 
 const crearUsuario = async (req, res = express.request) => {
   const { name, email, password } = req.body;
 
   try {
-    const usuario = new Usuario(req.body);
+    let usuario = await Usuario.findOne({ email: email });
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario ya existe con ese email",
+      });
+    }
+
+    usuario = new Usuario(req.body);
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
     await usuario.save();
+
     res.status(200).json({
       ok: true,
       usuario,
@@ -48,10 +59,35 @@ const crearUsuario = async (req, res = express.request) => {
   // });
 };
 
-const loginUsuario = (req, res = express.response) => {
-  res.json({
-    ok: true,
-  });
+const loginUsuario = async (req, res = express.request) => {
+  const { email, password } = req.body;
+
+  try {
+    let usuario = await Usuario.findOne({ email: email });
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe ",
+      });
+    }
+    const passwordValid = bcrypt.compareSync(password, usuario.password);
+    if (!passwordValid) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+    res.status(200).json({
+      ok: true,
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      error,
+    });
+  }
 };
 
 const revalidarToken = (req, res = express.response) => {
