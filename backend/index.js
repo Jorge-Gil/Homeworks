1d035769d2760d160c341122ee7e2469cf19afd3
Jorge-Gil <jorge.gil@uao.edu.co>
2023-05-09 20:06:18 -0500

    Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,11 @@ app.use(express.json());
 //Rutas
 app.use("/api/auth", require("./routes/auth"));
 
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
 //Escuchar el puerto 4000
 app.listen(process.env.PORT, () => {
   console.log("Servidor corriendo en el puerto", process.env.PORT);
